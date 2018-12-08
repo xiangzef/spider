058132e8d11476518b368c6a44fd6f98bd7e3c15
xzf <414395321@qq.com>
2018-12-08 11:37:44 +0800

    增加多线程request方法

--- shot1/MyThreat.py
+++ shot1/MyThreat.py
@@ -7,34 +7,34 @@ import re
 mutex_lock = threading.RLock()  # 互斥锁的声明
 threads = []#存放线程的数组，相当于线程池
 
-
-def read_url(path):
-    global rate
-    with open(path, encoding="utf-8") as js:
-        result = json.loads(js.read())
-        result_list = result['result']
-        i = 0
-        rate = round(len(result_list) / 7)
-        lst = numpy.arange(1, len(result_list), 1)
-        new_list = list(spider_mate.newlist(lst))
-        for res in result_list:
-            if i in new_list:
-                print('{:.2%}'.format(i / len(result_list)), end='■')
-            if i in numpy.arange(1, len(result_list), 3):
-                print('', end='■')
-            if res['y/n'] is not "错误链接":
-                answer_name = spider_mate.ask(res['url'])
-            res['y/n'] = answer_name[0]
-            if str(answer_name[0]).strip() == '在线' and str(res['name']).strip() == '大香蕉直播間_全球美女直播_大香蕉伊人網_大香蕉网_伊人在线大香蕉' :
-                result_list[i]['name'] = re.findall(r'[0-9a-zA-z_]+', str(answer_name[1]).strip().strip('\n'))[0]#正则表达式匹配大小写字母数字和下划线
-            i = 1 + i
-    with open(path, "w", encoding="utf-8") as js:
-        json.dump(result, js, ensure_ascii=False)
+#
+# def read_url(path):
+#     global rate
+#     with open(path, encoding="utf-8") as js:
+#         result = json.loads(js.read())
+#         result_list = result['result']
+#         i = 0
+#         rate = round(len(result_list) / 7)
+#         lst = numpy.arange(1, len(result_list), 1)
+#         new_list = list(spider_mate.newlist(lst))
+#         for res in result_list:
+#             if i in new_list:
+#                 print('{:.2%}'.format(i / len(result_list)), end='■')
+#             if i in numpy.arange(1, len(result_list), 3):
+#                 print('', end='■')
+#             if res['y/n'] is not "错误链接":
+#                 answer_name = spider_mate.ask(res['url'])
+#             res['y/n'] = answer_name[0]
+#             if str(answer_name[0]).strip() == '在线' and str(res['name']).strip() == '大香蕉直播間_全球美女直播_大香蕉伊人網_大香蕉网_伊人在线大香蕉' :
+#                 result_list[i]['name'] = re.findall(r'[0-9a-zA-z_]+', str(answer_name[1]).strip().strip('\n'))[0]#正则表达式匹配大小写字母数字和下划线
+#             i = 1 + i
+#     with open(path, "w", encoding="utf-8") as js:
+#         json.dump(result, js, ensure_ascii=False)
 
 
 
 class MyThread(threading.Thread):  # 线程处理函数
-    def __init__(self , i, res, new_list,length):
+    def __init__(self, i, res, new_list, length):
         threading.Thread.__init__(self)  # 线程类必须的初始化
         self.i = i  # 将传递过来的name构造到类中的name
         self.res = res  # 将传递过来的name构造到类中的name
@@ -45,17 +45,15 @@ class MyThread(threading.Thread):  # 线程处理函数
         # 声明在类中使用全局变量
         # 仅能有一个线程↓↓↓↓↓↓↓↓↓↓↓↓
         if self.res['y/n'] is not "错误链接":
+            self.res['y/n'] = ''
             answer_name = spider_mate.ask(self.res['url'])
         self.res['y/n'] = answer_name[0]
         if str(answer_name[0]).strip() == '在线' and str(self.res['name']).strip() == '大香蕉直播間_全球美女直播_大香蕉伊人網_大香蕉网_伊人在线大香蕉' :
             self.res['name'] = re.findall(r'[0-9a-zA-z_]+', str(answer_name[1]).strip().strip('\n'))[0]#正则表达式匹配大小写字母数字和下划线
         # 仅能有一个线程↑↑↑↑↑↑↑↑↑↑↑↑
-        mutex_lock.acquire()
-        if self.i in self.new_list:
-            print('{:.2%}'.format(self.i / self.length), end='■')
-        if self.i in numpy.arange(1, self.length, 3):
-            print('', end='■')
-        mutex_lock.release()
+        # mutex_lock.acquire()
+        #
+        # mutex_lock.release()
     def result(self):
         try:
             return dict(self.res)  # 如果子线程不使用join方法，此处可能会报没有self.result的错误
@@ -69,10 +67,18 @@ def call_thr(i, res, new_list,length):
 
 def start_thr(result):
     for t in threads:#让线程池中的所有数组开始
+        t.setDaemon(True)
         t.start()
+    print(threading.active_count())
     for t in threads:
         t.join()#等待所有线程运行完毕才执行一下的代码\
     i = 0
+    while threading.active_count() > 1:
+        i = 0
     for t in threads:
+        if t.i in t.new_list:
+            print('{:.2%}'.format(t.i / t.length), end='■')
+        if t.i in numpy.arange(1, t.length, 3):
+            print('', end='■')
         result['result'][i] = t.res
         i += 1
